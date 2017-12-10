075d8856ec0a144a6bc29f89d6de36d3f9aadf70
TMad <madsen.tyler@gmail.com>
2017-12-10 16:31:34 -0700

    built full comment functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,8 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 #models/article.rb
 class Article < ApplicationRecord
-
+  has_many :comments
 end
