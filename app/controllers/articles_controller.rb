5e5bfb4c3df067deef8e63b1a4a268760f011271
TMad <madsen.tyler@gmail.com>
2017-12-10 14:17:05 -0700

    add edit functionality and flash messages for edit/create/destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :destroy, :edit, :update]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -16,19 +17,39 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
-    Article.destroy(params[:id])
+    @article.destroy
+
+    flash.notice = "Article '#{@article.title}' has been Deleted!"
+
 
     redirect_to articles_path
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
   end
 
   private
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 
